1372db6b77c29041e098e6b7fe562f70b39cb541
Thiago de Mello Bueno <thiago@madeofpeople.org>
2018-06-07 19:15:37 -0400

    filtering wip

---
+++ src/models/filters.js
@@ -0,0 +1,32 @@
+import Axios from "axios";
+import { API_ROOT } from '../paths.js'
+
+const filters = {
+  state: {
+  },
+  reducers: {
+    onFilterOpenChanged() {
+
+    },
+    onFilterSortFieldChanged() {
+
+    },
+    onFilterSortOrientationChanged() {
+
+    },
+    onAddFilterQuery() {
+
+    },
+    onEditFilterQuery() {
+
+    },
+    onSetSearch() {
+
+    }
+  },
+  effects: {
+
+  }
+}
+
+export default filters
